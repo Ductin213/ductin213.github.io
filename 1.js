24ccb3348f0ff90804a12d112b7c13e49261eb83
Ductin213 <ductin2132019@gmail.com>
2021-09-24 21:59:08 +0700

    chinh sua hieu ung scroll den noi dung

--- 1.js
+++ 1.js
@@ -1,4 +1,11 @@
 ﻿ $(function(){
- 
-})  
+    
+    $(".btnxuong, .kn").click(function() {
+       $("body,html").animate({scrollTop:$('#khoiedu').offset().top}, 500); 
+    });
+
+    $(".sp").click(function() {
+       $("body,html").animate({scrollTop:$('#khoisp').offset().top}, 500); 
+    });
+});  
  
