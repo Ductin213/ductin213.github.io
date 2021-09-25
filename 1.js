6f3dbd05f1b7c1bda9370735de03f5241dc8c4b9
Ductin213 <ductin2132019@gmail.com>
2021-09-25 20:52:42 +0700

    xu li nav comment

--- 1.js
+++ 1.js
@@ -7,5 +7,8 @@
     $(".sp").click(function() {
        $("body,html").animate({scrollTop:$('#khoisp').offset().top}, 500); 
     });
+    $(".cm").click(function() {
+       $("body,html").animate({scrollTop:$('#khoicomment').offset().top}, 500); 
+    });
 });  
  
