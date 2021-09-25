a4b0b4480a135c53977f1ffe96580860a6106125
Ductin213 <ductin2132019@gmail.com>
2021-09-25 21:10:51 +0700

    chinh sua 1.js

--- 1.js
+++ 1.js
@@ -1,14 +1,17 @@
 ﻿ $(function(){
     
     $(".btnxuong, .kn").click(function() {
-       $("body,html").animate({scrollTop:$('#khoiedu').offset().top}, 500); 
+       $("body,html").animate({scrollTop:$('#khoiedu').offset().top}, 500);
+       return false; 
     });
 
     $(".sp").click(function() {
        $("body,html").animate({scrollTop:$('#khoisp').offset().top}, 500); 
+       return false;
     });
     $(".cm").click(function() {
        $("body,html").animate({scrollTop:$('#khoicomment').offset().top}, 500); 
+       return false;
     });
 });  
  
